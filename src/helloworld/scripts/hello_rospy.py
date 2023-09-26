6933f7afee6f050ad0734dcf1bc6d207a25ee91d
hayden <haohaoalt@163.com>
2023-09-26 23:57:39 +0800

    update 0926

---
+++ src/helloworld/scripts/hello_rospy.py
@@ -0,0 +1,7 @@
+#! /usr/bin/env python
+import rospy
+
+if __name__ == "__main__":
+    rospy.init_node("Hello")
+    rospy.loginfo("Hello World!!!!")
+    rospy.loginfo("Hello VScode Python ....")
--- src/helloworld/src/hello_ros.cpp
+++ src/helloworld/src/hello_ros.cpp
@@ -1,9 +1,13 @@
 #include "ros/ros.h"
 int main(int argc, char *argv[]){
+    setlocale(LC_CTYPE, "zh_CN.utf8");
+    setlocale(LC_ALL, "");
+
     //init
     ros::init(argc,argv,"hello");
     //node handle
     ros::NodeHandle n;
     ROS_INFO("hello world");
+    ROS_INFO("Hello VSCode!!!哈哈哈哈哈哈哈哈哈哈");
     return 0;
 }
